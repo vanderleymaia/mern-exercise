c80a3883068edd65ca43630ea581c8b9b7df13ac
Vanderley Maia <vanderleymaia@gmail.com>
2020-06-15 17:43:29 -0300

    first commit

---
+++ backend/models/exercise.model.js
@@ -0,0 +1,25 @@
+const mogoose = require('mongoose')
+
+const Schema = mogoose.Schema
+
+const exerciseSchema = new Schema
+(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
